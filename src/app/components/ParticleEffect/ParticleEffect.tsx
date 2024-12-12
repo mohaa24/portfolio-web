bb099d5c7e481ec7242625caf8404580d3c5a53b
mohaa24 <msmathnan@gmail.com>
2024-12-13 00:06:04 +0530

    particle colors update on darkmode

--- src/app/components/ParticleEffect/ParticleEffect.tsx
+++ src/app/components/ParticleEffect/ParticleEffect.tsx
@@ -1,17 +1,18 @@
 "use client";
 
+import { useAppContext } from "@/app/context/appContext";
 import Particles from "@/components/ui/particles";
 
 import * as React from "react";
 
 const AnimatedCanvas: React.FC = ({}) => {
-
+const {darkMode}= useAppContext();
   return (
     <Particles
         className="w-full h-full"
         quantity={800}
         ease={100}
-        color={'#ffff'}
+        color={darkMode? '#ffff':'#fff1122'}
         refresh
       />
   );
